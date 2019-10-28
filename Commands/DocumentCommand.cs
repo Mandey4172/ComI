53175955972dda5edd338cdd40439417100d4f20
Krzysztof Madej <Mandej4172@gmail.com>
2019-10-29 00:09:39 +0100

    Allow to find code elements in selection
    Commit adds possibility to find code elements (namespaces, classes,
    functions, etc.) inside selection in editor. This allows user to
    select elements which we document.
    
    Add methods to DocementCommand:
     - DocumentAsync - documents selected code element in editor.
     - SingleLineSelection - returns the list of code elements when a 
      single line is selected.
     - MultiLineSelection - returns the list of code elements if multiple 
     lines are selected.
     - GetSelectedItemsBetweenPoints - returns list of code element 
      between two edit points in document.
     - IsSelectionContaining - checks whether the compartments between 
     the first and second and third and fourth argument have a common 
     part. If so, the method returns the truth.
     - FindNameEnd - finds text point where is end  of code element name.

--- Commands/DocumentCommand.cs
+++ Commands/DocumentCommand.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.VCCodeModel;
 using Task = System.Threading.Tasks.Task;
 
 namespace ComI
 {
     /// <summary>
-    /// Command handler
+    /// DocumentCommand is used by the IDE to run on the code elements 
+    /// (namespace, classes, functions, etc.) documentation functionality.
     /// </summary>
     internal sealed class DocumentCommand
     {
@@ -86,20 +92,145 @@ namespace ComI
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
+        private async void Execute(object sender, EventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            await DocumentAsync();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task DocumentAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (await ServiceProvider.GetServiceAsync(typeof(DTE)) is DTE2 projectModel)
+            {
+                TextSelection selection = projectModel.ActiveDocument?.Selection as TextSelection;
+
+                if (selection.TopPoint != null && selection.BottomPoint != null &&
+                    ((selection.TopPoint.Line != selection.BottomPoint.Line) ||
+                    (selection.TopPoint.DisplayColumn != selection.BottomPoint.DisplayColumn)))
+                {
+                    List<VCCodeElement> selectetElements = MultiLineSelection(projectModel.ActiveDocument, selection);
+                    foreach (CodeElement element in selectetElements)
+                    {
+                        Debug.WriteLine(element.FullName);
+                    }
+                }
+                else if (selection.ActivePoint != null)
+                    Debug.WriteLine(SingleLineSelection(projectModel.ActiveDocument, selection.ActivePoint).Name);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="activeDocuemnt"></param>
+        /// <param name="selection"></param>
+        /// <returns></returns>
+        private List<VCCodeElement> MultiLineSelection(Document activeDocuemnt, TextSelection selection)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "DocumentCommand";
+            List<VCCodeElement> output;
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            EditPoint selectionTopPoint = selection.TopPoint.CreateEditPoint();
+            EditPoint selectionBottomPoint = selection.BottomPoint.CreateEditPoint();
+
+            CodeElements codeElements = activeDocuemnt.ProjectItem.FileCodeModel.CodeElements;
+            output = GetSelectedItemsBetweenPoints(codeElements, selectionTopPoint, selectionBottomPoint);
+
+            return output;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="codeElements"></param>
+        /// <param name="selectionTopPoint"></param>
+        /// <param name="selectionBottomPoint"></param>
+        /// <returns></returns>
+        private List<VCCodeElement> GetSelectedItemsBetweenPoints(CodeElements codeElements, EditPoint selectionTopPoint, EditPoint selectionBottomPoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            List<VCCodeElement> output = new List<VCCodeElement>();
+
+            for (int i = 1; i <= codeElements.Count; i++)
+            {
+                if (codeElements.Item(i) is VCCodeElement codeElement)
+                {
+                    TextPoint elementStartPoint = codeElement.StartPointOf[vsCMPart.vsCMPartWholeWithAttributes, vsCMWhere.vsCMWhereDeclaration];
+                    TextPoint elementEndPoint = codeElement.EndPointOf[vsCMPart.vsCMPartWholeWithAttributes, vsCMWhere.vsCMWhereDeclaration];
+                    EditPoint elementNameEndPoint = FindNameEnd(elementStartPoint.CreateEditPoint()).CreateEditPoint();
+
+                    Debug.WriteLine("codeElements = " + codeElement.FullName);
+                    Debug.WriteLine("elementStartPoint = " + elementStartPoint.AbsoluteCharOffset);
+                    Debug.WriteLine("elementEndPoint = " + elementEndPoint.AbsoluteCharOffset);
+                    //Debug.WriteLine("elementStartPointHeader = " + elementStartPointHeader.AbsoluteCharOffset);
+                    Debug.WriteLine("elementNameEndPoint = " + elementNameEndPoint.AbsoluteCharOffset);
+                    Debug.WriteLine("selectionTopPoint = " + selectionTopPoint.AbsoluteCharOffset);
+                    Debug.WriteLine("selectionBottomPoint = " + selectionBottomPoint.AbsoluteCharOffset);
+                    Debug.WriteLine("");
+
+                    if (IsSelectionContaining(selectionTopPoint, selectionBottomPoint, elementStartPoint, elementEndPoint))
+                    {
+                        if (IsSelectionContaining(selectionTopPoint, selectionBottomPoint, elementStartPoint, elementNameEndPoint))
+                        {
+                            Debug.WriteLine("Added -> codeElements = " + codeElement.FullName);
+                            output.Add(codeElement);
+                        }
+                        output.AddRange(GetSelectedItemsBetweenPoints(codeElement.Children, selectionTopPoint, selectionBottomPoint));
+                    }
+                }
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selectionTop"></param>
+        /// <param name="selectionBottom"></param>
+        /// <param name="startPoint"></param>
+        /// <param name="endPoint"></param>
+        /// <returns></returns>
+        private bool IsSelectionContaining(EditPoint selectionTop, EditPoint selectionBottom, TextPoint startPoint, TextPoint endPoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return !(startPoint.GreaterThan(selectionBottom) || !endPoint.GreaterThan(selectionTop));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elementStartPoint"></param>
+        /// <returns></returns>
+        private TextPoint FindNameEnd(EditPoint elementStartPoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            EditPoint2 actualPoint = (EditPoint2)elementStartPoint.CreateEditPoint();
+            string text = actualPoint.GetText(1);
+            while (text != "{" && text != ";")
+            {
+                actualPoint.CharRight();
+                text = actualPoint.GetText(1);
+            }
+            return actualPoint.CreateEditPoint();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="activeDocuemnt"></param>
+        /// <param name="activePoint"></param>
+        /// <returns></returns>
+        private CodeElement SingleLineSelection(Document activeDocuemnt, TextPoint activePoint)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return activeDocuemnt?.ProjectItem?.FileCodeModel?.CodeElementFromPoint(activePoint, vsCMElement.vsCMElementVCBase) as CodeElement;
         }
     }
 }
